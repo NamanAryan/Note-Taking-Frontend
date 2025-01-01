fe5e18de5c0ada7485619dda9e047447fc246ea2
Naman Aryan <160505477+NamanAryan@users.noreply.github.com>
2025-01-01 12:49:10 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,2 @@
-console.log('All env variables:', process.env);  // See all available env vars
-console.log('Specific API URL:', process.env.REACT_APP_API_URL);
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 export { API_URL };
