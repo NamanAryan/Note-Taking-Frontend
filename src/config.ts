b211e46491db5f0df420e257305430a1096f2982
Naman Aryan <160505477+NamanAryan@users.noreply.github.com>
2025-01-01 12:47:11 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
+console.log('All env variables:', process.env);  // See all available env vars
+console.log('Specific API URL:', process.env.REACT_APP_API_URL);
 const API_URL = process.env.REACT_APP_API_URL;
-console.log('ENV Variable:', process.env.REACT_APP_API_URL); // Add this to debug
 export { API_URL };
